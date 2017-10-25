0fd953f99a18e3564b45c8a3f3d3ddc4e57393aa
Konoha-orz <517136675@qq.com>
2017-10-25 15:33:25 +0800

    直播间原型导入

--- LiveRoomWeb/src/main/java/com/niit/org/controller/LiveController.java
+++ LiveRoomWeb/src/main/java/com/niit/org/controller/LiveController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
